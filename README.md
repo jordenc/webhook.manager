ae3ec8a910db37a77a11fb96c2df157f156e2d35
Jorden Chamid <jchamid@totallyhosted.nl>
2016-05-02 15:04:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### Webhook Manager
+
+Make it easier to POST information to Homey from external sources, using Webhooks.
+
+You first need to create a Webhook at https://developers.athom.com/webhooks/
+Keep the URL open since you need the URL, ID and Secret.
---
+++ app.js
@@ -0,0 +1,80 @@
+"use strict";
+var webhookID;
+var triggeredEvent;
+
+var self = module.exports = {
+	init: function () {
+
+		// On triggered flow
+		Homey.manager('flow').on('trigger.event', function (callback, args) {
+
+			// Check if event triggered is equal to event send in flow card
+			if (args.event === triggeredEvent) {
+				callback(null, true);
+			}
+			else {
+				callback(true, null);
+			}
+		});
+
+		// Register initial webhook
+		if (Homey.manager("settings").get("url") && Homey.manager("settings").get("id") && Homey.manager("settings").get("secret")) {
+
+			// Register webhook
+			self.registerWebhook(Homey.manager("settings").get("id"), Homey.manager("settings").get("secret"));
+
+		}
+
+		// Listen for settings change
+		var counter = 0;
+		Homey.manager('settings').on('set', function () {
+			counter++;
+			if (counter == 3) {
+				// Register new webhook
+				self.registerWebhook(Homey.manager("settings").get('id'), Homey.manager("settings").get('secret'));
+
+				// Reset counter
+				counter = 0;
+			}
+		});
+	},
+	registerWebhook: function (id, secret, callback) {
+
+		// Register webhook
+		Homey.manager('cloud').registerWebhook(id, secret, {}, self.incomingWebhook,
+			function (err, result) {
+				if (err || !result) {
+				
+					Homey.log('registering webhook failed');
+					
+					// Return failure
+					if (callback)callback(true, null);
+				}
+				else {
+					// Unregister old webhook
+					if (webhookID && webhookID !== id) Homey.manager('cloud').unregisterWebhook(webhookID);
+					
+					Homey.log('registering webhook succeeded');
+					// Return success
+					if (callback)callback(null, true);
+				}
+			});
+
+		// Store used webhook internally
+		webhookID = id;
+	},
+	incomingWebhook: function (args) {
+
+		// Store triggered event
+		triggeredEvent = args.body.event;
+		
+		Homey.log('incoming webhook: ' + triggeredEvent + ' / ' + JSON.stringify(args));
+
+		// Trigger event
+		Homey.manager('flow').trigger('event', {
+			data1: args.body.data1,
+			data2: args.body.data2,
+			data3: args.body.data3
+		});
+	}
+};
